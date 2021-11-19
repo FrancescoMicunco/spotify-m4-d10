71822ca6f4f96b52b5c5138e293d020e740d8036
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-19 20:56:34 +0100

    dinamic subtitle

--- src/Components/SubTitleJumbo.jsx
+++ src/Components/SubTitleJumbo.jsx
@@ -1,11 +1,11 @@
-const SubTitleJumbo =()=>{
+const SubTitleJumbo =(props)=>{
 
 return (
 
 <div className="row" id="artist1">
   <div className="col-12 hp-sub-hero">
     <div className="d-flex justify-content-between mt-4 mb-3 mx-0">
-      <h4 id="recently-played-h4">insert value of input</h4>
+      <h4 id="recently-played-h4">{props.title}</h4>
       <p className="hp-tag light-gray-text font-weight-bold">SEE ALL</p>
     </div>
     <div
